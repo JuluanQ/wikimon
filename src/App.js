ccc6e51ce93ebba7662bb14946bc47bf739b296a
JuluanQ <juluan.quardon29000@gmail.com>
2022-04-28 11:39:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
+import Pokedex from 'pokedex-promise-v2';
 import './App.css';
 
 function App() {
+
+  fetch('https://pokeapi.co/api/v2/pokemon/44/')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error=> console.log(error))
+  
   return (
     <div className="App">
       <header className="App-header">
