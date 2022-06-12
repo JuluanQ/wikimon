4d9cb6bc6354dc6e804d24d28597d109e123a03c
JuluanQ <48562224+JuluanQ@users.noreply.github.com>
2022-06-12 23:51:20 +0200

    searchWorking w/ dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import 'antd/dist/antd.css'
 import { SearchOutlined, HomeOutlined, UserOutlined } from '@ant-design/icons'
-import { Menu, Dropdown, notification } from 'antd';
+import { Menu, Form, Input, AutoComplete } from 'antd';
 
 import '../assets/css/App.css'
-import { Navigate, NavLink, useNavigate, useLocation } from 'react-router-dom';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
 
 const style = {
     backgroundColor: "#242424",
@@ -24,21 +23,6 @@ const homelogoStyle = {
     position: "absolute",
     left: "3em",
 }
-
-
-
-/*
-const menu = (
-    <Menu style={style} theme="dark"
-        items={[
-            {
-                label: "Try typing p/... or t/... !",
-                key: 0,
-            }
-        ]}
-    />
-)
-*/
 
 /* Permet de rechercher sans les accents */
 const rules = {
@@ -85,13 +69,47 @@ const Header = () => {
     }
     const handleMenuClick = () => {
     }
+    const [options, setOptions] = useState([]);
+
+    const handleSearch = (value) => {
+        setOptions([])
+        if (value.length > 2) {
+            //enlever les deux premiers caractères
+            let search = replaceSpec(value.substring(2));
+
+            //setOptions pour afficher les résultats de possibilités
+            let values = []
+            possiblites.forEach((possibilité) => {
+                if (possibilité != undefined) {
+                    if (values.length < 5) {
+                        values.push({ value: possibilité })
+                    }
+                }
+            })
+            setOptions(
+                !value ? [] : values
+            )
+
+        }
+    }
+    const onSelect = (value) => {
+        let idsearch = 1
+        speciesNameId.forEach((name, id) => {
+            let tmp = replaceSpec(name).toLowerCase()
+            if (tmp == value) {
+                idsearch = id
+                //quitter la boucle
+                return false
+            }
+        })
+        navigate("/pokemon/" + idsearch)
+    };
 
     const navigate = useNavigate()
     const location = useLocation()
     const speciesNameId = useSelector((state) => state.dataPkmn.speciesNameId.payload)
-    const [species, setSpecies] = useState();
     const [search, setSearch] = useState("");
-    const [isValid, setIsValid] = useState(false);
+    const [possiblites, setPossiblites] = useState();
 
     const handleSearchOnChange = (event) => {
         var text = event.target.value
@@ -100,14 +118,16 @@ const Header = () => {
             case text.startsWith("p/"):
                 text = text.substring(2)
                 let possibilities = []
+                let valuePossibilities = []
                 speciesNameId.forEach((value, key) => {
                     let tmp = replaceSpec(value.toLowerCase())
                     if (tmp.includes(text)) {
                         possibilities.push(key)
+                        valuePossibilities.push(tmp)
                         searchMap.set(key, value)
                     }
                 });
-                console.log(searchMap)
+                setPossiblites(valuePossibilities)
                 if (possibilities.length == 1) {
                     setSearch("/pokemon/" + possibilities[0])
                 }
@@ -120,7 +140,6 @@ const Header = () => {
 
     useEffect(() => {
         setSearch("")
-        document.getElementById("searchInput").value = ""
     }, [location]);
 
 
@@ -130,21 +149,26 @@ const Header = () => {
                 <NavLink to="/" style={{ display: "flex" }}>
                     <HomeOutlined className='homeLogo' style={homelogoStyle} />
                 </NavLink>
-
-                <Dropdown overlay={menu} trigger={['click']} overlayStyle={style}>
-                    <div className="searchBar">
-                        <SearchOutlined className='searchLogo' />
-                        <form onSubmit={() => { navigate(search) }} autoComplete="off" action="javascript:void(0);">
-                            <input type="search"
+                <div className="searchBar">
+                    <SearchOutlined className='searchLogo' />
+                    <form onSubmit={() => { navigate(search); }} action="javascript:void(0);">
+                        <AutoComplete
+                            style={{ color: "#fff" }}
+                            dropdownClassName="autoCompletePkmn"
+                            options={options}
+                            onSelect={onSelect}
+                            onSearch={handleSearch}
+                        >
+                            <Input type="search"
                                 id='searchInput'
                                 className='searchBarInput'
                                 placeholder='Search on Wikimon'
                                 onChange={handleSearchOnChange}
                             />
-                        </form>
+                        </AutoComplete>
+                    </form>
 
-                    </div>
-                </Dropdown>
+                </div>
             </div>
         </div >
     );
